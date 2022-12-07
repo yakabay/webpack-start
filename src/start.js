c795c5634a6571dc97e1debac624d77efe1f64cf
yakabay <yakabay86@gmail.com>
2022-12-07 11:44:27 +0100

    install UI library

--- src/start.js
+++ src/start.js
@@ -2,6 +2,11 @@ import React from "react"
 import { createRoot } from "react-dom/client"
 import "./style.css"
 import ReactButton from "./app/react/reactButton";
+import setupLocatorUI from "@locator/runtime";
+
+if (process.env.NODE_ENV === "development") {
+    setupLocatorUI();
+}
 
 const App = () => {
     return <div>
